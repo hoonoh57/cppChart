6442cce16f04f85b65b0bd26cac13846eceec815
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-03 15:46:51 +0900

    Store generic chart viewport and crosshair state

--- ui/render_document_renderer.h
+++ ui/render_document_renderer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../render/chart_viewport.h"
 #include "../render/render_document.h"
 
 #include "imgui.h"
@@ -10,6 +11,10 @@ namespace trading::ui
     {
         std::uint64_t renderedRevision = 0;
         bool dirty = true;
+        render::ChartViewport viewport;
+        bool crosshairVisible = false;
+        EpochMillis crosshairTimestampMs = 0;
+        double crosshairValue = 0.0;
     };
 
     void DrawRenderDocument(

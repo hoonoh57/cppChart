79315105fd4b877380e7578487a6e72d51e8801f
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-03 23:58:20 +0900

    Expose OBV parity output channels

--- core/obv_indicator.h
+++ core/obv_indicator.h
@@ -2,7 +2,13 @@
 
 #include "indicator_engine.h"
 
+#include <cstddef>
+
 namespace trading::indicators
 {
+    constexpr std::size_t ObvValueOutput = 0;
+    constexpr std::size_t ObvSignalOutput = 1;
+    constexpr std::size_t ObvDirectionOutput = 2;
+
     bool RegisterObvIndicator(IndicatorRegistry& registry);
 }

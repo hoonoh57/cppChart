ab80e513d57808f68364c3d8a0a2f7f649fe5874
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-04 00:02:44 +0900

    Expose ADX output channel

--- core/adx_indicator.h
+++ core/adx_indicator.h
@@ -2,7 +2,11 @@
 
 #include "indicator_engine.h"
 
+#include <cstddef>
+
 namespace trading::indicators
 {
+    constexpr std::size_t AdxValueOutput = 0;
+
     bool RegisterAdxIndicator(IndicatorRegistry& registry);
 }

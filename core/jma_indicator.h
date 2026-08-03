3d0ae2cd030eadf50e9efedefea9fd2b9c1bd693
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-03 23:57:08 +0900

    Expose JMA parity output channels

--- core/jma_indicator.h
+++ core/jma_indicator.h
@@ -2,7 +2,14 @@
 
 #include "indicator_engine.h"
 
+#include <cstddef>
+
 namespace trading::indicators
 {
+    constexpr std::size_t JmaValueOutput = 0;
+    constexpr std::size_t JmaUpOutput = 1;
+    constexpr std::size_t JmaDownOutput = 2;
+    constexpr std::size_t JmaSlopeOutput = 3;
+
     bool RegisterJmaIndicator(IndicatorRegistry& registry);
 }

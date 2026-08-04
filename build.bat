2e2bef1d25438cec617ea58e7885369da6297a4a
hoonoh57 <36817134+hoonoh57@users.noreply.github.com>
2026-08-05 08:02:34 +0900

    Build practical symbol toolbar wrapper

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ cd /d %~dp0
 if not exist obj mkdir obj
 cl /nologo /std:c++17 /utf-8 /O2 /W3 /EHsc /MD /DUNICODE /D_UNICODE /D_WIN32_WINNT=0x0602 ^
    /I"imgui" /I"imgui\backends" ^
-   shell_main.cpp ^
+   shell_main_m82.cpp ^
    core\command_bus.cpp ^
    core\fault_policy.cpp ^
    core\json_lite.cpp ^
